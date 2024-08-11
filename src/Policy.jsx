e1774941afd63554a632ac510cebf35bf29695d8
Bishal Deb <102280193+bishald0013@users.noreply.github.com>
2024-08-11 11:19:31 +0530

    first commit

---
+++ src/Policy.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Policy() {
+  return (
+    <div className='container mt-3'>
+        Privacy Policy<br/><br/>
+
+        Last Updated: 11-08-2024<br/><br/>
+
+        Welcome to Swan Vendor. We are committed to protecting your privacy. This Privacy Policy explains how we collect, use, and share your personal information when you use our app, Swan Vendor App. <br/><br/>
+
+        By using our App, you agree to the collection and use of information in accordance with this policy.<br/><br/>
+
+        1. Information We Collect<br/><br/>
+        a. Personal Information<br/><br/> 
+
+        When you use our App, we may collect personal information that you provide to us, including but not limited to:<br/>
+        <ul>
+          <li>Name</li>
+          <li>Email address</li>
+          <li>Phone number</li>
+          <li>Location data</li>
+          <li>Zip Code</li>
+        </ul>
+    </div>
+  )
+}
